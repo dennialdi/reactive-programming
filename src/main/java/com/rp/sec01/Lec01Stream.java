24ba5547bf4809d14ac2c3c5cd3237c6949227e0
dennialdi <dennialdi@gmail.com>
2021-12-27 14:23:16 +0700

    section 1 - mono

--- src/main/java/com/rp/sec01/Lec01Stream.java
+++ src/main/java/com/rp/sec01/Lec01Stream.java
@@ -14,6 +14,7 @@ public class Lec01Stream {
                     return i * 2;
                 });
 
-        System.out.println(stream);
+        //System.out.println(stream);
+        stream.forEach(System.out::println);
     }
 }
---
+++ src/main/java/com/rp/sec01/Lec09AssignmentDemo.java
@@ -0,0 +1,17 @@
+package com.rp.sec01;
+
+import com.rp.courseutil.Util;
+import com.rp.sec01.assignment.FileService;
+
+public class Lec09AssignmentDemo {
+    public static void main(String[] args) {
+        FileService.read("file02.txt")
+                .subscribe(Util.onNext(), Util.onError(), Util.onComplete());
+
+//        FileService.write("file03.txt","This is file3")
+//                .subscribe(Util.onNext(),Util.onError(),Util.onComplete());
+
+        FileService.delete("file03.txt")
+                .subscribe(Util.onNext(), Util.onError(), Util.onComplete());
+    }
+}
